a0985556e9c436e30f26f710253201dfbd4cfb0b
Amir Mohammad Hosseini <amirmohammad2005hosseini@gmail.com>
2026-08-20 18:48:02 +0330

    type assignment and type inference

---
+++ 01-Typescript-Essentials/Session-02/basics.js
@@ -0,0 +1,5 @@
+"use strict";
+let userName; // Type Assignment
+let userAge = 21; // Type Inference
+userName = "Amir";
+console.log(userName, userAge);
---
+++ 01-Typescript-Essentials/Session-02/basics.ts
@@ -0,0 +1,6 @@
+let userName: string; // Type Assignment
+let userAge : number = 21; // Type Inference
+
+userName = "Amir";
+
+console.log(userName , userAge)
