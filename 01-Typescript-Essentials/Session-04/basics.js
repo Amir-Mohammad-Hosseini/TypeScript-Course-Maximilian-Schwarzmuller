cdd83a2e1ed0a24ad50819999a0f3833f3aa963c
Amir Mohammad Hosseini <amirmohammad2005hosseini@gmail.com>
2026-08-20 20:37:25 +0330

    Any and Union types

---
+++ 01-Typescript-Essentials/Session-04/basics.js
@@ -0,0 +1,11 @@
+"use strict";
+// Any Type
+let userDatas = "Amir";
+userDatas = {};
+userDatas = [];
+userDatas = false;
+console.log(userDatas);
+// Union Type
+let adminDatas = 21;
+adminDatas = "18-19 Day 1404";
+console.log(adminDatas);
---
+++ 01-Typescript-Essentials/Session-04/basics.ts
@@ -0,0 +1,19 @@
+// Any Type
+
+let userDatas : any = "Amir"
+
+userDatas = {}
+
+userDatas = []
+
+userDatas = false
+
+console.log(userDatas)
+
+// Union Type
+
+let adminDatas : string | number = 21
+
+adminDatas = "18-19 Day 1404"
+
+console.log(adminDatas)
