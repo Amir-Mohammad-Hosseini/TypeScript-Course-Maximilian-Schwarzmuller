eba07436ea00ba4e260dbd960861bcc7ed4a472f
Amir Mohammad Hosseini <amirmohammad2005hosseini@gmail.com>
2026-08-22 15:39:08 +0330

    Objects and Record types

---
+++ 01-Typescript-Essentials/Session-06/basics.js
@@ -0,0 +1,25 @@
+"use strict";
+// Object
+let userDatas = {
+    name: "Amir",
+    age: 21,
+    isAdmin: false,
+    hobbies: ["Cooking", "Playing", "Programming"],
+    address: {
+        country: "Iran",
+        city: "Tehran"
+    }
+};
+console.log(userDatas);
+// Object type's bug (must not be null or undefined)
+let productDatas = "Iphone 17 Pro Max";
+productDatas = 21; // !!!
+productDatas = ["p1", "p2", "p3"]; // !!!
+console.log(productDatas);
+// Record type, A flexible object
+let cartDatas;
+cartDatas = {
+    itemsCount: 5,
+    itemsName: "Cart Items..."
+};
+console.log(cartDatas);
