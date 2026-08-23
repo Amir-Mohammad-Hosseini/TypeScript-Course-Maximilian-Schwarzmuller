7ff198795ef213bec6db8f20f7ad0a833e9e477b
Amir Mohammad Hosseini <amirmohammad2005hosseini@gmail.com>
2026-08-23 19:37:33 +0330

    type casting

--- 01-Typescript-Essentials/Session-09/form.ts
+++ 01-Typescript-Essentials/Session-09/form.ts
@@ -6,4 +6,19 @@ if(!inputEl){
     throw new Error("Element not found!")
 }
 
+/*
+
+We get error here
+
 console.log(inputEl?.value)
+*/
+
+// Type Casting
+
+const inputElement = document.querySelector("#user-name") as HTMLInputElement | null
+
+if(!inputElement){
+    throw new Error("Element not found!")
+}
+
+console.log(inputElement?.value)
