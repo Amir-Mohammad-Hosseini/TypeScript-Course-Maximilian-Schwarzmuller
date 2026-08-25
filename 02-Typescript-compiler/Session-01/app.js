60407dec7097de7916b38a4a301ccb428f0307ac
Amir Mohammad Hosseini <amirmohammad2005hosseini@gmail.com>
2026-08-26 00:26:08 +0330

    Add tsconfig.json file and ts compiler

---
+++ 02-Typescript-compiler/Session-01/app.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const node_fs_1 = __importDefault(require("node:fs"));
+// fs.readFileSync()
+let userName;
+userName = "Amir";
+function add(a, b) {
+    return a + b;
+}
+console.log(add(2, 3));
+//# sourceMappingURL=app.js.map
