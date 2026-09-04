3129667ada6d6cdf7ca173861065c543e58e885f
Amir Mohammad Hosseini <amirmohammad2005hosseini@gmail.com>
2026-09-04 17:54:23 +0330

    another example of conditional types

--- 08-Deriving-Types-From-Types/Session-06/conditional.ts
+++ 08-Deriving-Types-From-Types/Session-06/conditional.ts
@@ -17,3 +17,18 @@ type GetElementType<T> = T extends any[] ? T[number] : T;
 type Example1 = GetElementType<StringArray>;
 
 type Example2 = GetElementType<typeof text>;
+
+
+type FullNamePerson = {firstName : string , lastName : string}
+type FullNameOrNothing<T> = T extends FullNamePerson ?string : never
+
+function getFullName<T extends object> (person : T)  {
+    if("firstName" in person && "lastName" in person && person.firstName && person.lastName){
+        return `${person.firstName} ${person.lastName}` as FullNameOrNothing<T>
+    }
+
+    throw new Error("No first name or last name found")
+}
+
+const name1 = getFullName({})
+const name2 = getFullName({firstName : "Amir" , lastName : "Hosseini"})
