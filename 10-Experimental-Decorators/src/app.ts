3dfe9421ff989653f02a6c38294260294c531987
Amir Mohammad Hosseini <amirmohammad2005hosseini@gmail.com>
2026-09-06 18:03:23 +0330

    create a decorator

--- 10-Experimental-Decorators/src/app.ts
+++ 10-Experimental-Decorators/src/app.ts
@@ -7,10 +7,12 @@ function Logger(logString: string) {
 
 function WithTemplate(template: string, hookId: string) {
   console.log("TEMPLATE FACTORY");
-  return function<T extends {new(...args : any[]) : {name : string}}> (originalConstructor: T) {
+  return function <T extends { new (...args: any[]): { name: string } }>(
+    originalConstructor: T,
+  ) {
     console.log("Rendering template...");
     return class extends originalConstructor {
-      constructor(..._ : any[]) {
+      constructor(..._: any[]) {
         super();
         const hookEl = document.getElementById(hookId);
         if (hookEl) {
@@ -93,3 +95,31 @@ class Product {
 
 const p1 = new Product("Book", 18);
 const p2 = new Product("Book 2", 19);
+
+function Autobind (_ : any , _2 : string , descriptor : PropertyDescriptor) {
+  const originalMethod =descriptor.value
+  const adjDescriptor : PropertyDescriptor = {
+    configurable : true ,
+    enumerable : false , 
+    get () {
+      const bindFn = originalMethod.bind(this)
+    }
+  }
+
+  return adjDescriptor
+
+}
+
+class Printer {
+  message = "This works!";
+
+  @Autobind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const p = new Printer()
+
+const button = document.querySelector("button")
+button?.addEventListener("click" , p.showMessage)
