9cbee54a3757f995792e4469eed760a284fdc6b0
Amir Mohammad Hosseini <amirmohammad2005hosseini@gmail.com>
2026-09-11 17:46:18 +0330

    start drag-and-drop project

---
+++ 11-Drag-And-Drop-Project/src/app.ts
@@ -0,0 +1,26 @@
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element : HTMLFormElement
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input",
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importdHTMLContent = document.importNode(
+      this.templateElement.content,
+      true,
+    );
+    this.element = importdHTMLContent.firstElementChild as HTMLFormElement
+    this.element.id = "user-input"
+    this.attach()
+  }
+
+  private attach (){
+    this.hostElement.insertAdjacentElement("afterbegin" , this.element)
+  }
+}
+
+const projectInput = new ProjectInput()
