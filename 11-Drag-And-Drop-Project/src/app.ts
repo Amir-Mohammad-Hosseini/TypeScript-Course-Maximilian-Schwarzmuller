149dd6e34706219fbc39fad00abf03e8c6f0a161
Amir Mohammad Hosseini <amirmohammad2005hosseini@gmail.com>
2026-09-12 16:43:31 +0330

    manage state of application with singletons

--- 11-Drag-And-Drop-Project/src/app.ts
+++ 11-Drag-And-Drop-Project/src/app.ts
@@ -1,3 +1,41 @@
+// Project state management class
+
+class ProjectState {
+  private listeners: any[] = [];
+  private projects: any[] = [];
+  private static instance: ProjectState;
+
+  private constructor() {}
+
+  static getInstance() {
+    if (this.instance) {
+      return this.instance;
+    }
+    this.instance = new ProjectState();
+    return this.instance;
+  }
+
+  addListener(listenerFn: Function) {
+    this.listeners.push(listenerFn);
+  }
+
+  addProject(title: string, description: string, numOfPeople: number) {
+    const newProject = {
+      id: crypto.randomUUID(),
+      title,
+      description,
+      people: numOfPeople,
+    };
+
+    this.projects.push(newProject);
+    for (const listenerFn of this.listeners) {
+      listenerFn(this.projects.slice());
+    }
+  }
+}
+
+const projectSate = ProjectState.getInstance();
+
 // Validation logic
 
 interface Validatable {
@@ -60,6 +98,60 @@ const autobind = (_: any, _2: string, descriptor: PropertyDescriptor) => {
   return adjDescriptor;
 };
 
+// ProjectList Class
+
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+  assignedProjects: any[];
+
+  constructor(private type: "active" | "finished") {
+    this.templateElement = document.getElementById(
+      "project-list",
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+    this.assignedProjects = [];
+
+    const importdHTMLContent = document.importNode(
+      this.templateElement.content,
+      true,
+    );
+    this.element = importdHTMLContent.firstElementChild as HTMLElement;
+    this.element.id = `${type}-projects`;
+
+    projectSate.addListener((projects: any[]) => {
+      this.assignedProjects = projects;
+      this.renderProjects()
+    });
+
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderProjects() {
+    const listEl = document.getElementById(
+      `${this.type}-projects-list`,
+    )! as HTMLUListElement;
+    for (const prjItem of this.assignedProjects) {
+      const listItem = document.createElement("li");
+      listItem.textContent = prjItem.title;
+      listEl?.appendChild(listItem);
+    }
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent =
+      this.type.toUpperCase() + "PROJECTS";
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+}
+
 // ProjectInput Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
@@ -101,24 +193,24 @@ class ProjectInput {
     const enteredDescription = this.descriptionInputElement.value;
     const enteredPeople = this.peopleInputElement.value;
 
-    const titleValidatable : Validatable = {
-      value : enteredTitle,
-      required : true
-    }
-    const descriptionValidatable : Validatable = {
-      value : enteredDescription,
-      required : true,
-      minLength : 5
-    }
-    const peopleValidatable : Validatable = {
-      value : +enteredPeople,
-      required : true,
-      min : 1
-    }
+    const titleValidatable: Validatable = {
+      value: enteredTitle,
+      required: true,
+    };
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
+    };
+    const peopleValidatable: Validatable = {
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+    };
 
     if (
-      !validate(titleValidatable) &&
-      !validate(descriptionValidatable) &&
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
       !validate(peopleValidatable)
     ) {
       alert("Invalid input, please try again!");
@@ -141,7 +233,7 @@ class ProjectInput {
     const userInput = this.gatherUserInput();
     if (Array.isArray(userInput)) {
       const [title, description, people] = userInput;
-      console.log(title, description, people);
+      projectSate.addProject(title, description, people);
       this.clearInputs();
     }
   }
@@ -156,3 +248,5 @@ class ProjectInput {
 }
 
 const projectInput = new ProjectInput();
+const activeProjectList = new ProjectList("active");
+const finishedProjectList = new ProjectList("finished");
