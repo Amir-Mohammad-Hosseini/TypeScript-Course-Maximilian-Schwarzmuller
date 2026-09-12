4fddc6abbb33855936b045c78be5516ad06698b4
Amir Mohammad Hosseini <amirmohammad2005hosseini@gmail.com>
2026-09-12 16:55:58 +0330

    more information about classes and custom types

--- 11-Drag-And-Drop-Project/src/app.ts
+++ 11-Drag-And-Drop-Project/src/app.ts
@@ -1,8 +1,22 @@
-// Project state management class
+// Project  Type
 
+enum ProjectStatus {Active , Finished}
+
+class Project {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public people: number,
+    public status: ProjectStatus,
+  ) {}
+}
+
+// Project state management class
+type Listener = (items : Project[]) => void
 class ProjectState {
-  private listeners: any[] = [];
-  private projects: any[] = [];
+  private listeners: Listener[] = [];
+  private projects: Project[] = [];
   private static instance: ProjectState;
 
   private constructor() {}
@@ -15,17 +29,18 @@ class ProjectState {
     return this.instance;
   }
 
-  addListener(listenerFn: Function) {
+  addListener(listenerFn: Listener) {
     this.listeners.push(listenerFn);
   }
 
   addProject(title: string, description: string, numOfPeople: number) {
-    const newProject = {
-      id: crypto.randomUUID(),
+    const newProject = new Project(
+     crypto.randomUUID(),
       title,
       description,
-      people: numOfPeople,
-    };
+       numOfPeople,
+       ProjectStatus.Active
+    )
 
     this.projects.push(newProject);
     for (const listenerFn of this.listeners) {
@@ -104,7 +119,7 @@ class ProjectList {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLElement;
-  assignedProjects: any[];
+  assignedProjects: Project[];
 
   constructor(private type: "active" | "finished") {
     this.templateElement = document.getElementById(
@@ -120,9 +135,9 @@ class ProjectList {
     this.element = importdHTMLContent.firstElementChild as HTMLElement;
     this.element.id = `${type}-projects`;
 
-    projectSate.addListener((projects: any[]) => {
+    projectSate.addListener((projects: Project[]) => {
       this.assignedProjects = projects;
-      this.renderProjects()
+      this.renderProjects();
     });
 
     this.attach();
@@ -133,6 +148,7 @@ class ProjectList {
     const listEl = document.getElementById(
       `${this.type}-projects-list`,
     )! as HTMLUListElement;
+    listEl.innerHTML = "";
     for (const prjItem of this.assignedProjects) {
       const listItem = document.createElement("li");
       listItem.textContent = prjItem.title;
