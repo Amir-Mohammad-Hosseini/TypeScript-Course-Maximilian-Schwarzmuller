1b9b4a07f06b9884f95adf6246c2f4398a4d0eb2
Amir Mohammad Hosseini <amirmohammad2005hosseini@gmail.com>
2026-09-13 15:12:02 +0330

    problem in import namespaces

---
+++ 11-Drag-And-Drop-Project/src/components/base-component.ts
@@ -0,0 +1,42 @@
+namespace App {
+      // Component Base class
+
+  export abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+    templateElement: HTMLTemplateElement;
+    hostElement: T;
+    element: U;
+
+    constructor(
+      templateId: string,
+      hostElementId: string,
+      insertAtStart: boolean,
+      newElementId?: string,
+    ) {
+      this.templateElement = document.getElementById(
+        templateId,
+      )! as HTMLTemplateElement;
+      this.hostElement = document.getElementById(hostElementId)! as T;
+
+      const importdHTMLContent = document.importNode(
+        this.templateElement.content,
+        true,
+      );
+      this.element = importdHTMLContent.firstElementChild as U;
+      if (newElementId) {
+        this.element.id = newElementId;
+      }
+
+      this.attach(insertAtStart);
+    }
+
+    private attach(insertAtBeggining: boolean) {
+      this.hostElement.insertAdjacentElement(
+        insertAtBeggining ? "afterbegin" : "beforeend",
+        this.element,
+      );
+    }
+
+    abstract configure(): void;
+    abstract renderContent(): void;
+  }
+}
---
+++ 11-Drag-And-Drop-Project/src/state/project-state.ts
@@ -0,0 +1,60 @@
+// Project state management class
+namespace App {
+  type Listener<T> = (items: T[]) => void;
+
+  class State<T> {
+    protected listeners: Listener<T>[] = [];
+
+    addListener(listenerFn: Listener<T>) {
+      this.listeners.push(listenerFn);
+    }
+  }
+
+  export class ProjectState extends State<Project> {
+    private projects: Project[] = [];
+    private static instance: ProjectState;
+
+    private constructor() {
+      super();
+    }
+
+    static getInstance() {
+      if (this.instance) {
+        return this.instance;
+      }
+      this.instance = new ProjectState();
+      return this.instance;
+    }
+
+    addProject(title: string, description: string, numOfPeople: number) {
+      const newProject = new Project(
+        crypto.randomUUID(),
+        title,
+        description,
+        numOfPeople,
+        ProjectStatus.Active,
+      );
+
+      this.projects.push(newProject);
+      this.updateListeners();
+    }
+
+    moveProject(projectId: string, newStatus: ProjectStatus) {
+      const movedProject = this.projects.find(
+        (project) => project.id === projectId,
+      );
+      if (movedProject && movedProject.status !== newStatus) {
+        movedProject.status = newStatus;
+        this.updateListeners();
+      }
+    }
+
+    private updateListeners() {
+      for (const listenerFn of this.listeners) {
+        listenerFn(this.projects.slice());
+      }
+    }
+  }
+
+  export const projectSate = ProjectState.getInstance();
+}
