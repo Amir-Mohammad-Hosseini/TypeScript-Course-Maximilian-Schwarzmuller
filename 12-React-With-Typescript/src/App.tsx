76f17995a6fa7f70141c703cb6394f72be448cbe
Amir Mohammad Hosseini <amirmohammad2005hosseini@gmail.com>
2026-09-15 16:34:22 +0330

    special children prop

--- 12-React-With-Typescript/src/App.tsx
+++ 12-React-With-Typescript/src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import Header from "./components/Header"
+import goalsImg from "./assets/goals.jpg"
+
 
 const App = () => {
   return (
-    <>
-     <h1>Hello World!</h1> 
-    </>
+    <main>
+      <Header image={{src : goalsImg , alt : "A list of goals"}}>
+        <h1>Your Course Goals</h1>
+      </Header>
+    </main>
   )
 }
 
--- 12-React-With-Typescript/src/components/Header.tsx
+++ 12-React-With-Typescript/src/components/Header.tsx
@@ -1,16 +1,20 @@
+import type { ReactNode } from "react";
+
 interface HeaderProps {
-    image : {
-        src : string,
-        alt : string
-    }
+  image: {
+    src: string;
+    alt: string;
+  };
+  children?: ReactNode;
 }
 
-const Header = ({image} : HeaderProps) => {
+const Header = ({ image, children }: HeaderProps) => {
   return (
     <header>
-        <img src={image.src} alt={image.alt} />
+      <img src={image.src} alt={image.alt} />
+      {children}
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
