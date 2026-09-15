40df9451caed577c06789bfcfeddd9bdad864d0d
Amir Mohammad Hosseini <amirmohammad2005hosseini@gmail.com>
2026-09-15 16:53:09 +0330

    manage goals state

--- 12-React-With-Typescript/src/App.tsx
+++ 12-React-With-Typescript/src/App.tsx
@@ -1,19 +1,21 @@
-import Header from "./components/Header"
-import goalsImg from "./assets/goals.jpg"
-import CourseGoals from "./components/CourseGoals"
-
+import Header from "./components/Header";
+import goalsImg from "./assets/goals.jpg";
+import CourseGoals from "./components/CourseGoals";
+import { useState } from "react";
 
 const App = () => {
+  const [goals, setGoals] = useState([
+    { id: "g1", title: "Learn TS", description: "Learn TS from the ground up" },
+    { id: "g2", title: "Practice TS", description: "Practice working with TS" },
+  ]);
   return (
     <main>
-      <Header image={{src : goalsImg , alt : "A list of goals"}}>
+      <Header image={{ src: goalsImg, alt: "A list of goals" }}>
         <h1>Your Course Goals</h1>
       </Header>
-      <CourseGoals goals={[{id : "g1" , title : "Learn TS" , description : "Learn TS from the ground up"} , 
-        {id : "g2" , title : "Practice TS" , description : "Practice working with TS"}
-      ]} />
+      <CourseGoals goals={goals} />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
