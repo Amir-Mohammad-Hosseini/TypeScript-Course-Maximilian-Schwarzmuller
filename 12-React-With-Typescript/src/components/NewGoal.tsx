444b8abd898815803ee26b2eb233cca2e245099f
Amir Mohammad Hosseini <amirmohammad2005hosseini@gmail.com>
2026-09-15 17:17:43 +0330

    manage submit forms

--- 12-React-With-Typescript/src/components/NewGoal.tsx
+++ 12-React-With-Typescript/src/components/NewGoal.tsx
@@ -1,11 +1,26 @@
-
+import type { SubmitEventHandler } from "react";
 
 const NewGoal = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
 
-export default NewGoal
+    const handleSubmitNewGoal : SubmitEventHandler<HTMLFormElement> = (event) => {
+        event.preventDefault()
+        console.log(event.target)
+    }
+  return (
+    <form onSubmit={handleSubmitNewGoal}>
+      <p>
+        <label htmlFor="goal">Your goal</label>
+        <input type="text" id="goal" />
+      </p>
+      <p>
+        <label htmlFor="summary">Your goal</label>
+        <input type="text" id="summary" />
+      </p>
+      <p>
+        <button type="submit">Add goal</button>
+      </p>
+    </form>
+  );
+};
+
+export default NewGoal;
