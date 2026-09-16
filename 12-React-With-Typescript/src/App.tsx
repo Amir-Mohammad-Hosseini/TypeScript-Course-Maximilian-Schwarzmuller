b768edb262f84106317db82392de6e287011638d
Amir Mohammad Hosseini <amirmohammad2005hosseini@gmail.com>
2026-09-16 15:39:05 +0330

    finish react+typescript project

--- 12-React-With-Typescript/src/App.tsx
+++ 12-React-With-Typescript/src/App.tsx
@@ -2,11 +2,16 @@ import Header from "./components/Header";
 import goalsImg from "./assets/goals.jpg";
 import CourseGoals from "./components/CourseGoals";
 import { useState } from "react";
+import NewGoal from "./components/NewGoal";
+
+type Goal = {
+  id: string;
+  title: string;
+  description: string;
+};
 
 const App = () => {
-  const [goals, setGoals] = useState([
-    { id: "g1", title: "Learn TS", description: "Learn TS from the ground up" },
-    { id: "g2", title: "Practice TS", description: "Practice working with TS" },
+  const [goals, setGoals] = useState<Goal[]>([
   ]);
 
   const handleDeleteGoal = (goalId : string) => {
@@ -16,12 +21,17 @@ const App = () => {
     })
   }
 
+  const handleAddGoal = (title : string , description : string) => {
+    setGoals((prevGoals) => [...prevGoals , {id : `g${goals.length + 1}` , title , description}])
+  }
+
   return (
     <main>
       <Header image={{ src: goalsImg, alt: "A list of goals" }}>
         <h1>Your Course Goals</h1>
       </Header>
       <CourseGoals goals={goals} onDelete={handleDeleteGoal} />
+      <NewGoal onAddGoal={handleAddGoal} />
     </main>
   );
 };
--- 12-React-With-Typescript/src/components/NewGoal.tsx
+++ 12-React-With-Typescript/src/components/NewGoal.tsx
@@ -1,20 +1,35 @@
-import type { SubmitEventHandler } from "react";
+import { useRef, type SubmitEventHandler } from "react";
 
-const NewGoal = () => {
+interface NewGoalProps {
+  onAddGoal: (title : string , description : string) => void;
+}
 
-    const handleSubmitNewGoal : SubmitEventHandler<HTMLFormElement> = (event) => {
-        event.preventDefault()
-        console.log(event.target)
-    }
+const NewGoal = ({ onAddGoal }: NewGoalProps) => {
+  const goalRef = useRef<HTMLInputElement>(null);
+  const summaryRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmitNewGoal: SubmitEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+
+    const enteredGoal = goalRef.current!.value;
+    const enteredSummary = summaryRef.current!.value;
+
+    // validation ...
+
+    onAddGoal(enteredGoal, enteredSummary);
+
+    goalRef.current!.value = "";
+    summaryRef.current!.value = "";
+  };
   return (
     <form onSubmit={handleSubmitNewGoal}>
       <p>
         <label htmlFor="goal">Your goal</label>
-        <input type="text" id="goal" />
+        <input type="text" id="goal" ref={goalRef} />
       </p>
       <p>
         <label htmlFor="summary">Your goal</label>
-        <input type="text" id="summary" />
+        <input type="text" id="summary" ref={summaryRef} />
       </p>
       <p>
         <button type="submit">Add goal</button>
